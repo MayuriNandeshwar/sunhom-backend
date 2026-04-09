89a5a7425fa356d9ce4bcf74037626cf2dca8991
MayuriNandeshwar <mayurinandeshwar60@gmail.com>
2026-04-09 13:57:14 +0530

    remove EmailService temporarily

--- src/main/java/com/sunhom/common/email/EmailService.java
+++ src/main/java/com/sunhom/common/email/EmailService.java
@@ -1,32 +1,32 @@
-package com.sunhom.common.email;
+// package com.sunhom.common.email;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.mail.SimpleMailMessage;
+// import org.springframework.mail.javamail.JavaMailSender;
+// import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class EmailService {
+// @Service
+// @RequiredArgsConstructor
+// public class EmailService {
 
-    private final JavaMailSender mailSender;
+// private final JavaMailSender mailSender;
 
-    public void sendNewsletterWelcome(String email) {
+// public void sendNewsletterWelcome(String email) {
 
-        SimpleMailMessage message = new SimpleMailMessage();
+// SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setFrom("info@sunhom.in");
-        message.setTo(email);
+// message.setFrom("info@sunhom.in");
+// message.setTo(email);
 
-        message.setSubject("Welcome to SUNHOM");
+// message.setSubject("Welcome to SUNHOM");
 
-        message.setText(
-                "Welcome to SUNHOM.\n\n" +
-                        "Thank you for subscribing to our fragrance newsletter.\n\n" +
-                        "You will now receive exclusive offers and launches.\n\n" +
-                        "Team SUNHOM\n" +
-                        "www.sunhom.in");
+// message.setText(
+// "Welcome to SUNHOM.\n\n" +
+// "Thank you for subscribing to our fragrance newsletter.\n\n" +
+// "You will now receive exclusive offers and launches.\n\n" +
+// "Team SUNHOM\n" +
+// "www.sunhom.in");
 
-        mailSender.send(message);
-    }
-}
+// mailSender.send(message);
+// }
+// }
